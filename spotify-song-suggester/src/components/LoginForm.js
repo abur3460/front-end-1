1576074e357303d13d8be1b4595b801a82964aaa
JoeOs <JoeOs@github.com>
2020-06-24 22:30:27 -0400

    added Login component

--- spotify-song-suggester/src/components/LoginForm.js
+++ spotify-song-suggester/src/components/LoginForm.js
@@ -1,58 +1,61 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import { useHistory } from 'react-router-dom'
 import axiosWithAuth from '../utils/axiosWithAuth';
-import { useHistory } from 'react-router-dom';
 
-const LoginForm = () => {
-    const [credentials2, setCredentials2] = useState({
-        email: '',
-        password: '',
-    })
 
+
+const Login = () => {
     const history = useHistory();
+    
+  const onSubmit = (val) => {
+    alert(JSON.stringify(val));
+    axiosWithAuth()
+    .post( "https://spotifysongsbw.herokuapp.com/api/user/login" ,val).then(resp => {localStorage.setItem('token', resp.data.token) 
+    history.push("/hub")})
+    .catch(err => console.log(err))
+    
+};
 
-    const handleChanges = e => {
-        setCredentials2({
-            ...credentials2,
-            [e.target.name]: e.target.value
-        })
-    }
-
-    const loginUser = e => {
-        e.preventDefault();
-        axiosWithAuth()
-          .post('/user/login', credentials2)
-          .then(res => {
-              localStorage.setItem('token', res.data.token)
-              history.push('/hub');
-              console.log(res)
-          })
-          .catch(err => console.log('look!', err.response))
-    }
-
-    return (
-        <form onSubmit={loginUser}>
-
-            <input
-             type='email'
-             name='email'
-             value={credentials2.email}
-             placeholder='Email here'
-             onChange={handleChanges}
-            />
-
-            <input
-             type='password'
-             name='password'
-             placeholder='Password here'
-             value={credentials2.password}
-             onChange={handleChanges}
-            />
-
-            <button>Login!</button>
+  const initialValues = {
+    email: "",
+    password: ""
+};
 
 
-        </form>
-    )
+  const SignupSchema =
+    Yup.object().shape({
+    email: Yup.string().required("add a email"),
+    password: Yup.string().required("add a password")
+  });
+
+  return (
+    <div className="login">
+      <h1>Log In</h1>
+      <Formik {...{initialValues, onSubmit}} 
+      validationSchema={SignupSchema}>
+        {() => (
+          <Form className="form">
+            <div className="input-box">
+              <label htmlFor="email">email</label>
+              <Field type="text" id="email" name="email" />
+            </div>
+
+            <div className="input-box">
+              <label htmlFor="password">Password</label>
+              <Field type="password" id="password" name="password" />
+            </div>  
+            <div>
+              <button type="submit" >Submit</button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  )
 }
 
-export default LoginForm
+
+export default Login;
